45f5262c05975e014657345872df199ea0f27d44
Habib Ullah Khan Jamee <43148756+HabibJamee@users.noreply.github.com>
2020-11-17 11:39:59 +0600

    Update Detection.py

--- Detection.py
+++ Detection.py
@@ -5,3 +5,22 @@ import numpy as np
 import pytesseract
 from PIL import Image
 import matplotlib.pyplot as plt
+
+img = cv2.imread('',cv2.IMREAD_COLOR) #file name
+
+img = cv2.resize(img, (620,480) )
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #convert to grey scale
+gray = cv2.bilateralFilter(gray, 11, 17, 17) #Blur to reduce noise
+edged = cv2.Canny(gray, 30, 200) #Perform Edge detection
+
+cnts = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:10]
+screenCnt = None
+implot=plt.imshow(img)
+plt.show()
+implot=plt.imshow(gray)
+plt.show()
+implot=plt.imshow(edged)
+plt.show()
